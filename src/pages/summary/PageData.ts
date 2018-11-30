b96f214f71d380c69410e464a2b38574c111043e
Vikranth Thati <vithati@microsoft.com>
2018-11-30 19:18:30 +0530

    Updated azdevops-kube-summary package.

--- src/pages/summary/PageData.ts
+++ src/pages/summary/PageData.ts
@@ -3,7 +3,7 @@
     Licensed under the MIT license.
 */
 
-import { KubeResourceType, KubeServiceBase } from "azdevops-kube-summary/dist/Contracts/Contracts";
+import { KubeResourceType, KubeServiceBase } from "azdevops-kube-summary/dist/Contracts/KubeServiceBase";
 
 export class PageDataService extends KubeServiceBase {
     public fetch(resourceType: KubeResourceType): Promise<any> {
