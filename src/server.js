980654859b603c2b6a76c7df0407d120e1c006e1
Madhu V <madhuv@microsoft.com>
2019-01-22 15:33:21 +0530

    removed a duplicate log statement

--- src/server.js
+++ src/server.js
@@ -28,7 +28,6 @@ const getStatefulSets = (namespace, pretty) => { return k8sAppApiClient && k8sAp
 var app = express();
 app.use(express.static('dest'));
 
-console.log('Using port:' + port);
 console.log('Address to use for browsing: http://localhost:' + port);
 app.get('/', function (req, res) {
     res.redirect('/login');
